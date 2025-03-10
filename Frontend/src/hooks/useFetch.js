d419a05b629997247c19dea69ab5008f30eaf7df
Rahim Adnan <hafizrahim3011@gmail.com>
2025-03-10 10:20:54 +0500

    implement course api

---
+++ Frontend/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url, method = "GET") => {
+    const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log(url, method);
+            try {
+                let response = await axios({
+                    url,
+                    method,
+
+                });
+
+                if (response.status >= 200 && response.status < 300) {
+                    setData(response.data.data);
+                } else {
+                    throw new Error(`HTTP error: ${response.status}`);
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        if (url) fetchData(); // Ensure URL is provided before calling
+
+    }, [url, method]); // Correct dependencies
+
+    return { data };
+};
+
+export default useFetch;
+
--- Frontend/src/pages/Course.jsx
+++ Frontend/src/pages/Course.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Layouts/Navbar";
 import CoursesSection from "../components/UI/CoursesSection";
 import CourseCard from "../components/Layouts/Card";
 import Footer from "../components/Layouts/Footer";
+import useFetch from "../hooks/useFetch";
 const Course = () => {
+  let { data } = useFetch("http://localhost:3000/api/v1/courses", "GET");
+  console.log(data);
   return (
     <div className="min-h-screen w-full flex flex-col">
       <Navbar />
       <CoursesSection />
-      <CourseCard />
+      <CourseCard data={data || []} />
       <Footer />
     </div>
   );
